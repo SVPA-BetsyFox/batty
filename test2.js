ff75fa38bdfe0c7304efc7e837739e9c369fb0fe
Betsy Fox <betsy.fox@sony.com>
2018-04-18 16:19:20 -0700

    closure-ified

--- test2.js
+++ test2.js
@@ -1,152 +1,134 @@
-const { spawn, spawnSync } = require('child_process');
-spawnSync('adb', ['connect', '172.30.7.97']);
-const child = spawn('adb', ['shell']);
-let PROMPT = /^shell@[A-Za-z0-9-_.]+:\/ \$/;
 
 
-let clean = (x) => x.toString().replace("\r", "");
-let finish = function() {
-  process.exit(0);
-}
+var Jatty = function(ip="172.30.7.97") {
+  const { spawn, spawnSync } = require('child_process');
+  const PROMPT = /shell@[A-Za-z0-9-_.]+:\/ \$/;
+  const DEFAULT_TIMEOUT = 10000;
 
-let altcap = 0;
-let packages = (x) => {
-  console.log("DOING SHIT");
-  return x.toString().split("\n").map(x => x.split("").map(y => (altcap++ % 2 == 0) ? y.toUpperCase() : y.toLowerCase()).join(""));
-}
+  var adb, timeout;
+  var running = false;
+  var temp = [];
+  var output = [];
 
-var usable = false;
-let mode = "output";
+  // var clean = (x="") => x.toString().replace("\r", "");
+  var finish = () => process.exit(0);
+  var stop_timer = () => clearTimeout(timeout);
 
-let tasks = [{cmd: "pm list packages -f", clean: clean, cb: packages }, {cmd: "exit", clean: finish}];
-let current_task = {};
-let temp = [];
-let output = [];
-
-let last_output_ts = Date.now();
+  var reset_timer = function(t=DEFAULT_TIMEOUT) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => stop(), t);
+  }
 
 
+  var pause = () => running = false;
+  var play = () => current_task ? send(current_task["cmd"]) : send();
+  var queue = (cmd, cb=(x)=>x, clean=(x)=>x) => tasks.push({cmd: cmd, cb: cb, clean: clean});
 
-let timeout = setTimeout(() => {
-  flush();
-  console.log(output);
-  process.exit(1)},
-  20000);
 
-let reset_timeout = function(t=5000) {
-  console.log("Timeout counter was reset!");
-  clearTimeout(timeout);
-  timeout = setTimeout(() => {
+  var stop = function(stopcode=0) {
+    console.log("-    -   -  - - -----= Stop was called- flushing and quitting =----- - -  -   -    -");
     flush();
-    console.log("----------------------------------------------------------------------");
-    console.log(output);
-    process.exit(1);
-  },
-  t)
-}
-
-
-let flush = function() {
-  last_output_ts = Date.now();
-  process.stdout.write("###\n");
-  if ("clean" in current_task) {
-    // console.log(`CALLING CB ON "${temp}", which is of type ${typeof temp}`);
-    temp = current_task["clean"](temp);
+    console.log("LAST OUTPUT:");
+    console.log(output)
+    process.exit(stopcode);
   }
-  console.log("" + temp);
-  if (temp.join != undefined) temp = temp.join("")
-  output.push(temp);
-  temp = [];
-  console.log(("cb" in current_task) ? current_task["cb"](output) : output);
-  if ((current_task = tasks.shift()) !== undefined && "cmd" in current_task) {
-    send(current_task["cmd"]);
+
+
+  var connect = function(msg) {
+    if (msg !== undefined) console.log(msg);
+    status = spawnSync('adb', ['connect', ip]).toString();    
+    adb = spawn('adb', ['shell']);
+    adb.stdout.on('readable', () => recieve(adb.stdout.read()));
+    adb.stdout.on('end', () => connect("RECONNECTING..."));
+    adb.stdout.on('finish', () => console.error('All writes are now complete.'));
+  }
+
+  var tasks = [];//[{cmd: "pm list packages -f", clean: clean }, {cmd: "exit", clean: finish}];
+  var current_task = {};
+  var output = [];
+
+  var last_output_ts = Date.now();
+
+  var reset_timer = function(t=10000) {
+    // console.log("Timeout counter was reset!");
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      flush();
+      console.log("----------------------------------------------------------------------");
+      console.log(output);
+      process.exit(1);
+    },
+    t)
+  }
+
+
+  var flush = function() {
+    stop_timer();
+    if (current_task && "clean" in current_task) {
+      console.log('THERE IS A CLEAN FUNCTION, CALLING IT')
+      // console.log(`CALLING CB ON "${temp}", which is of type ${typeof temp}`);
+      temp = current_task["clean"](temp.join(""));
+    }
+    console.log("TEMP STUFF AT FLUSHING TIEM: " + temp);
+    // if (temp.join != undefined) temp = temp.join("")
+    output = temp;
+    temp = [];
+    console.log('-'.repeat(80))
+    console.log((current_task && "cb" in current_task) ? current_task["cb"](output) : output);
+    console.log('-'.repeat(80))
+    if (running && (current_task = tasks.shift()) !== undefined && "cmd" in current_task) {
+      send(current_task["cmd"]);
+    }
+  }
+
+
+  var recieve = function(data) {
+    // console.log(`ms since last data is: ${Date.now() - last_output_ts}`);
+    process.stdout.write("=");
+    if ((data == undefined) || (data == null)) {
+      if (Date.now() - last_output_ts > 10000) flush();
+      if (Date.now() - last_output_ts > 30000) process.quit("OH GOD TIMEOUT");
+      return;
+    }
+    if (PROMPT.test(data)) flush();
+    else temp.push(data.toString());
+
+    last_output_ts = Date.now();
+  }
+
+
+  var send = function(data="") {
+    running = true;
+    reset_timer();
+    adb.stdin.write(data + "\n");
+    if (data == "exit") stop();
+  }
+
+  return {
+    connect: connect,
+    send: send,
+    recieve: recieve,
+    flush: flush,
+    stop_timer: stop_timer,
+    pause: pause,
+    play: play,
+    queue: queue,
+    stop: stop,
+    finish: finish,
+
+    reset_timer: reset_timer,
   }
 }
 
-let process_data = function(data) {
-  // console.log(`ms since last data is: ${Date.now() - last_output_ts}`);
-  process.stdout.write("=");
-  if ((data == undefined) || (data == null)) {
-    if (Date.now() - last_output_ts > 10000) flush();
-    if (Date.now() - last_output_ts > 30000) process.quit("OH GOD TIMEOUT");
-    return;
-  }
-  if (PROMPT.test(data)) flush();
-  else temp.push(data.toString());
-
-  last_output_ts = Date.now();
-
-
-  // if (PROMPT.test(data) && tasks.length > 0) {
-  //   current_task = tasks.shift()
-  //   if ("cmd" in current_task) send(current_task["cmd"]);
-  // }
-  // else if (mode == "output" && data.trim().length > 0) temp.push(data.trim());
+let clean_lines = (x) => {
+  console.log('CLEAN LINES WAS CALLED');
+  return x.split(/\r?\n/).map(y => y.trim());
 }
 
-let send = function(data) {
-  child.stdin.write(data + "\n");
-}
-
-child.stdout.on('readable', () => {
-  usable = true;
-  let i = 0;
-  process_data(child.stdout.read());
-  // if (raw) console.log(raw.toString());
-  // console.log(`${typeof raw}: "${raw}"`);
-  // if (raw) console.log("line 43" + process_data(raw.toString()));
-  // lines = raw ? raw.toString().split("\n") : []
-  // lines.forEach(line => process_data(line));
-});
-
-child.stdout.on('end', () => {
-  // console.log('end');
-});
-
-child.stdout.on('finish', () => {
-  // console.log("output is------------------>\n" + output);
-  console.error('All writes are now complete.');
-  finish();
-});
-
-// child.stdout.pause();
-// while(!usable) { child.stdin.write("ls"); console.log("waiting...")}
-// child.stdin.write("ls\n");
-// child.stdin.write("exit\n");
-// child.stdout.resume();
-// child.stdin.write("ls\n");
-// child.stdout.on('readable', function() {
-//   fnord = child.stdout.read();
-//   console.log(fnord);
-//   // console.log(data.toString());
-//   // if (!sending) {
-//   //   usable = (PROMPT.test(data.toString()));
-//   //   console.log(usable);
-//   //   writing = true;
-//   //   console.log(data.toString());
-//   //   result += data.toString();
-//   //   writing = false;
-//   // }
-//   // return true;
-// });
-// ls = () => {
-//   sending = true;
-//   while(writing);
-//   child.stdin.write("ls -a\n");
-//   sending = false;
-// }
-
-// exit = () => {
-//   sending = true;
-//   while(writing);
-//   // while(!usable);
-//   child.stdin.write("exit\n");
-//   sending = false;
-// }
-
-// ls();
-// exit();
-
-// while ((fnord = child.stdout.read()) != undefined) {
-//   console.log(fnord);
-// }
+j = new Jatty();
+j.connect();
+j.queue("ls", (x)=>x, clean_lines);
+j.play();
+// j.stop();
+// j.pause();
+// j.queue();
